717e11e9221830c1b93bcce7e39c160f98b76092
cody budge <codybudge@gmail.com>
2018-05-07 16:09:27 -0600

    final change on everything

--- playersControllers.js
+++ playersControllers.js
@@ -7,40 +7,44 @@ function PlayersControllers() {
         //draw the search bars
         console.log(data)
         document.getElementById('search-form').innerHTML = `
-        <form class="form-inline d-flex justify-content-center" onsubmit="app.controllers.playersControllers.getPlayersName(event)">
+        <div class="row">
+        <form class="form-inline col-4"onsubmit="app.controllers.playersControllers.getPlayersName(event)">
         <div class="form-group">
         <input type="text" class="form-control" name="playersData" placeholder="Player Name" />
-        <button type="submit" class="btn btn-primary" id="get-player-button">Serch Players</button>
+        <button type="submit" class="btnColor" id="get-player-button">Serch Players</button>
         </div>
         </form>
-        <form class="form-inline d-flex justify-content-center" onsubmit="app.controllers.playersControllers.getPlayersTeam(event)">
+        <form class="form-inline col-4" onsubmit="app.controllers.playersControllers.getPlayersTeam(event)">
          <div class="form-group">
          <input type="text" class="form-control" name="playersData" placeholder="Player Team" />
-         <button type="submit" class="btn btn-primary" id="get-player-button">Serch Players</button>
+         <button type="submit" class="btnColor" id="get-player-button">Serch Players</button>
          </div>
          </form>
-         <form class="form-inline d-flex justify-content-center" onsubmit="app.controllers.playersControllers.getPlayersPosition(event)">
+         <form class="form-inline col-4" onsubmit="app.controllers.playersControllers.getPlayersPosition(event)">
          <div class="form-group">
          <input type="text" class="form-control" name="playersData" placeholder="Player Position" />
-         <button type="submit" class="btn btn-primary" id="get-player-button">Serch Players</button>
+         <button type="submit" class="btnColor" id="get-player-button">Serch Players</button>
          </div>
-         </form>`
+         </div>
+         </form>
+
+         `
     }
 
 
     function drawPlayTeam(players) {
-        var template = '<h1>Your NFL Team</h1>';
+        var template = '<div class="col-12 textColor"><h1>Your NFL Team</h1></div>';
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
             template += `
             <div class="col-3">
-            <div class="card" style="width: 18rem;">
+            <div class="cardBg">
             <img class="card-img-top" src="${player.photo}" alt="Card image cap">
             <div class="card-body">
             <h5 class="card-title">Name:${player.fullname}</h5>
             <p class="card-text">Position:${player.position}</p>
             <p class="card-text">Team:${player.pro_team}</p>
-            <a onclick="app.controllers.playersControllers.removeFromTeam(${player.id})" class="btn btn-primary text-white">Remove</a>
+            <button onclick="app.controllers.playersControllers.removeFromTeam(${player.id})" class="btnColor">Remove</button>
             </div>
             </div>
             </div>
@@ -50,12 +54,12 @@ function PlayersControllers() {
     }
 
     function drawPlayerList(players) {
-        var template = '<h1>List of Players</h1>';
+        var template = '<div class="col-12"><h1>List of Players</h1></div>';
         for (let i = 0; i < players.length; i++) {
             const player = players[i];
             template += `
             <div class="col-3">
-            <div class="card">
+            <div class="cardBg">
             <img class="card-img-top" src="${player.photo}" alt="Card image cap">
             <div class="card-header">
             Team:${player.pro_team}
@@ -63,7 +67,7 @@ function PlayersControllers() {
             <div class="card-body">
             <h5 class="card-title">Name:${player.fullname}</h5>
             <p class="card-title">Position:${player.position}</p>
-            <a onclick="app.controllers.playersControllers.addPlayer(${player.id})" class="btn btn-primary text-white">Add Player</a>
+            <button onclick="app.controllers.playersControllers.addPlayer(${player.id})" class="btnColor">Add Player</button>
             </div>
             </div>
             </div>
